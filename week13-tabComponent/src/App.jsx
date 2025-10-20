86c29dc316a092a0f5e6564bcfaf80d57a3d561e
mohammad sadra nafe <sadranafe7@gmail.com>
2025-10-20 21:24:35 +0330

    feat: add the DUMMYDATA in App.jsx and add the UI in Tabs.jsx components

--- week13-tabComponent/src/App.jsx
+++ week13-tabComponent/src/App.jsx
@@ -1,7 +1,16 @@
 const App = () => {
+  const DummyData = [
+    { label : 'Tab 1' , header : 'content 1' , content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, cum!' },
+    { label : 'Tab 2' , header : 'content 2' , content: 'Lorem ipsum dolor sit amet.' },
+    { label : 'Tab 3' , header : 'content 3' , content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est quos ipsa architecto temporibus odio magnam quibusdam unde repudiandae autem eos.' },
+    { label : 'Tab 4' , header : 'content 4' , content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eveniet, doloribus assumenda perspiciatis commodi reprehenderit praesentium quis animi facilis sint minima harum debitis distinctio magnam libero quae, neque optio. Dicta!' },
+  ]
+  
   return (
     <>
-      <h1 className = "bg-red-200">hello world</h1>
+      <div className = "w-full h-screen flex flex-wrap justify-center items-center">
+          
+      </div>
     </>
   )
 }
---
+++ week13-tabComponent/src/components/Tabs/Tabs.jsx
@@ -0,0 +1,12 @@
+const Tabs = () => {
+    return (
+        <>
+            <div className = "bg-linear-to-br from-sky-300/30 to-blue-600/50 h-60 shadow-lg w-1/2 rounded-4xl p-7">
+                <h1 className = "capitalize text-center font-semibold text-2xl">tabs component with react</h1>
+                
+            </div>
+        </>
+    );
+};
+
+export default Tabs;
