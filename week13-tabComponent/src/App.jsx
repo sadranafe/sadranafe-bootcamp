7d1090abb2c24d9141a69fe6462b1456d06181e2
mohammad sadra nafe <sadranafe7@gmail.com>
2025-10-20 21:46:49 +0330

    feat: create and Add Tabs.jsx in App.jsx

--- week13-tabComponent/src/App.jsx
+++ week13-tabComponent/src/App.jsx
@@ -1,3 +1,5 @@
+import Tabs from "./components/Tabs/Tabs"
+
 const App = () => {
   const DummyData = [
     { label : 'Tab 1' , header : 'content 1' , content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum, cum!' },
@@ -5,11 +7,11 @@ const App = () => {
     { label : 'Tab 3' , header : 'content 3' , content: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Est quos ipsa architecto temporibus odio magnam quibusdam unde repudiandae autem eos.' },
     { label : 'Tab 4' , header : 'content 4' , content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eveniet, doloribus assumenda perspiciatis commodi reprehenderit praesentium quis animi facilis sint minima harum debitis distinctio magnam libero quae, neque optio. Dicta!' },
   ]
-  
+
   return (
     <>
       <div className = "w-full h-screen flex flex-wrap justify-center items-center">
-          
+          <Tabs tabs = {DummyData}/>
       </div>
     </>
   )
--- week13-tabComponent/src/components/Tabs/Tabs.jsx
+++ week13-tabComponent/src/components/Tabs/Tabs.jsx
@@ -1,9 +1,15 @@
-const Tabs = () => {
+import { useState } from "react";
+import TabLists from "./TabLists";
+import TabPanels from "./TabPanels";
+
+const Tabs = ({ tabs }) => {
+    const [activeIndex , setActiveIndex] = useState(0)
     return (
         <>
-            <div className = "bg-linear-to-br from-sky-300/30 to-blue-600/50 h-60 shadow-lg w-1/2 rounded-4xl p-7">
-                <h1 className = "capitalize text-center font-semibold text-2xl">tabs component with react</h1>
-                
+            <div className = "bg-linear-to-br from-sky-300/30 to-blue-600/50 h-64 shadow-lg w-1/2 rounded-4xl p-7">
+                <h1 className = "capitalize text-center font-semibold text-3xl">tabs component with react</h1>
+                <TabLists tabs = {tabs} activeIndex = {activeIndex}  setActiveIndex = {setActiveIndex}/>
+                <TabPanels tabs = {tabs} activeIndex = {activeIndex}/>
             </div>
         </>
     );
