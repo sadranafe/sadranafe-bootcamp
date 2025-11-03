06c561214553d90ddd6bfdcd5bffeb178a075483
mohammad sadra nafe <sadranafe7@gmail.com>
2025-11-03 18:47:28 +0330

    chore : remove the fake dummydata of the contacts in contactApp.jsx file

--- week14-contactApp/src/components/contact/ContactApp.jsx
+++ week14-contactApp/src/components/contact/ContactApp.jsx
@@ -5,12 +5,7 @@ import Model from "../model";
 import Toast from "../Toast";
 
 const ContactApp = () => {
-    const [DummyContacts, setDummyContacts] = useState([
-        { id: 0 , name : 'sadra' , email : 'sadranafe7@gmail.com' , phoneNumber : '09109919520' , selected : false },
-        { id: 1 , name : 'john' , email : 'john@gmail.com' , phoneNumber : '09101234567' , selected : false },
-        { id: 2 , name : 'reza' , email : 'reza24@gmail.com' , phoneNumber : '09129214020' , selected : false },
-        { id: 3 , name : 'sarina' , email : 'sarinal83@gmail.com' , phoneNumber : '09190719570' , selected : false },
-    ])
+    const [DummyContacts, setDummyContacts] = useState([])
 
     const [modelIsOpen , setModelIsOpen] = useState(false);
     const [selectedContact , setSelectedContact] = useState({}); // for selecting a contact
