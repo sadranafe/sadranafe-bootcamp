b6cb60147848c5293c1f44e992619f4a5bb625de
mohammad sadra nafe <sadranafe7@gmail.com>
2025-11-03 10:47:56 +0330

    style : fix minor UI and CSS issues

--- week14-contactApp/src/components/contactPanel/ContactsHeaderPanel.jsx
+++ week14-contactApp/src/components/contactPanel/ContactsHeaderPanel.jsx
@@ -4,8 +4,8 @@ import Sort from '../Sort';
 const ContactsHeaderPanel = () => {
     return (
         <>
-            <div>
-                <div>
+            <div className = 'mb-5'>
+                <div className = 'flex items-center justify-between'>
                     <SearchBar />
 
                     <button title = 'add contact' className = 'group cursor-pointer w-8 h-8 flex justify-center items-center rounded-full bg-white/45 border-white/80 backdrop-blur-xl shadow-[0_5px_10px_rgba(0,0,0,0.05)] border text-gray-500 transition-all duration-200 hover:w-20 hover:bg-sky-500 hover:text-white'>
@@ -16,7 +16,7 @@ const ContactsHeaderPanel = () => {
 
                 <div className = 'flex items-center justify-between mt-5'>
                     <h1 className = 'capitalize text-xl font-semibold'>contacts</h1>
-                    <div>
+                    <div className = 'flex gap-3'>
                         <button className = 'text-blue-500 hover:border-blue-500 px-3 cursor-pointer rounded-full border border-transparent transition-all'>select</button>
                         <Sort/>
                     </div>
