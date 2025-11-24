acf50d63d056c17db18e0a9b203e58c84295d157
mohammad sadra nafe <sadranafe7@gmail.com>
2025-11-24 19:37:09 +0330

    style: change the style of the toast

--- week14-contactApp/src/components/Toast.jsx
+++ week14-contactApp/src/components/Toast.jsx
@@ -15,17 +15,17 @@ const Toast = () => {
     },[isFired])
 
     const typeStyles = {
-        success : 'bg-emerald-400',
-        error : 'bg-rose-400',
-        info : 'text-sky-400',
+        success : 'bg-emerald-300/80',
+        error : 'bg-rose-300/80',
+        info : 'bg-transparent text-black/75',
     }
     return (
         <>
-            <div className = {`${isFired ? 'opacity-100 block visible' : 'hidden opacity-0 invisible'} z-30 shadow-md absolute flex top-1 right-2 min-w-[150px] h-10 justify-start items-center gap-2.5 p-1.5 px-2.5 border-dashed border border-gray-300 rounded-2xl transition-all`}>
-                <p className = {`font-medium text-lg w-[25px] h-[25px] rounded-full flex justify-center items-center ${typeStyles[type]}`}>
-                    {type === 'success' && <i className = "bx bx-check text-white"></i>}
-                    {type === 'error' && <i className = "bx bx-x text-white"></i>}
-                    {type === 'info' && <i className = "bx bx-alert-circle text-xl"></i>}
+            <div className = {`${isFired ? 'opacity-100 block visible' : 'hidden opacity-0 invisible'} z-30 shadow-lg absolute flex top-3.5 min-w-40 backdrop-blur-xl bg-linear-to-r from-[#cacacab8] to-neutral-100 h-10 justify-start items-center gap-2.5 p-5 px-3 border border-white/50 rounded-2xl transition-all`}>
+                <p className = {`font-medium text-lg w-[25px] h-[25px] rounded-md flex justify-center items-center ${typeStyles[type]}`}>
+                    {type === 'success' && <i className = "bx bx-check"></i>}
+                    {type === 'error' && <i className = "bx bx-x"></i>}
+                    {type === 'info' && <i className = "bx bx-alert-triangle text-xl"></i>}
                 </p>
 
                 <p className = "capitalize text-xs">{content}</p>
