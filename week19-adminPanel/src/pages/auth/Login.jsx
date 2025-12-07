3fc4eb9a5fdfdde15c909b342ff57545a0cdbd25
mohammad sadra nafe <sadranafe7@gmail.com>
2025-12-08 01:30:40 +0330

    feat(login-ui): add the layout of the login page

--- week19-adminPanel/src/pages/auth/Login.jsx
+++ week19-adminPanel/src/pages/auth/Login.jsx
@@ -1,9 +1,19 @@
+import { Link } from "react-router-dom";
+import Form from "../../components/Form";
 
 const Login = () => {
     return (
-        <>
-            
-        </>
+        <Form titleForm = 'فرم ورود'>
+            <div className = "w-full flex flex-col flex-wrap justify-center items-center content-center gap-3">
+                <input type = "text" placeholder = "نام کاربری" className = "border border-white bg-neutral-100/60 backdrop-blur-xl rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.05)] outline-none p-2 px-3 w-10/12"/>
+                <input type = "password" placeholder = "رمز عبور" className = "border border-white bg-neutral-100/60 backdrop-blur-xl rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.05)] outline-none p-2 px-3 w-10/12"/>
+            </div>
+
+            <div className = "w-full flex flex-wrap justify-center items-center gap-4">
+                <button type = "submit" className = "text-white bg-blue-500 rounded-lg py-2 w-10/12">ورود</button>
+                <Link to = '/auth/register' className = "text-blue-500 text-xs transition-all border-b border-transparent hover:border-b-blue-400">ایجاد حساب کاربری !</Link>
+            </div>
+        </Form>
     );
 };
 
