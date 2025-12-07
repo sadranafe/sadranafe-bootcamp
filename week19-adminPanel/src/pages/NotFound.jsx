d81ee026ffc87576d973a684b7add9ec5af26a1d
mohammad sadra nafe <sadranafe7@gmail.com>
2025-12-07 20:09:41 +0330

    feat(notFound-ui): add notFound page layout and responsive

--- week19-adminPanel/src/pages/NotFound.jsx
+++ week19-adminPanel/src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>oooppps.... 404</h1>
+        <div className = "w-full h-screen flex flex-wrap flex-col justify-center items-center">
+            <div className = "text-center">
+                <h1 className = "font-bold text-6xl max-sm:text-5xl">404</h1>
+                <h2 className = "text-sm mt-2">There is no fish</h2>
+            </div>
+            
+            <img src = "/notFoundImg.png" alt = "not found" className = "w-lg"/>
         </div>
     );
 };
