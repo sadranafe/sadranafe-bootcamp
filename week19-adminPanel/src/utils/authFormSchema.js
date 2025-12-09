5bf1a187e500400b25095581935c833e7351ed82
mohammad sadra nafe <sadranafe7@gmail.com>
2025-12-09 23:48:32 +0330

    refactor : change the language of http errors into persian

--- week19-adminPanel/src/utils/authFormSchema.js
+++ week19-adminPanel/src/utils/authFormSchema.js
@@ -3,18 +3,18 @@ import * as yup from 'yup';
 const authFormSchema = (hasRepeatedPassword = false) => {
     return yup.object({
         userName : yup.string()
-        .min(3 , 'Must be at least 3 characters or more')
-        .max(15 , 'Must be 15 characters or less')
-        .required('userName is required'),
+        .min(3 , 'باید حداقل 3 کاراکتر یا بیشتر باشد')
+        .max(15 , 'باید حداکثر 15 کاراکتر یا کمتر باشد')
+        .required('نام کاربری الزامی میباشد'),
         password : yup.string()
-        .min(8 , 'Password must be at least 8 characters')
-        .matches(/[0-9]/, "Password must contain at least one number")
-        .matches(/[a-zA-Z]/, "Password must contain at least one letter")
-        .required('password is required'),
+        .min(8 , 'رمز عبور باید حداقل 8 کاراکتر باشد')
+        .matches(/[0-9]/, "رمز عبور باید حداقل شامل یک عدد باشد")
+        .matches(/[a-zA-Z]/, "رمز عبور باید شامل حروف باشد")
+        .required('رمز عبور الزامی میباشد'),
         ...(hasRepeatedPassword && {
             repeatedPassword : yup.string()
-            .oneOf([yup.ref('password')] , 'Passwords do not match')
-            .required('confirm password is required')
+            .oneOf([yup.ref('password')] , 'رمز عبور تکرار شده با رمز عبور وارد شده یکسان نیست')
+            .required('رمز عبور رو دوباره وارد کنید')
         })
     })
 }
