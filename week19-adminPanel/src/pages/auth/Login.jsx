48b98ebe9d655d44b8efc835d8c095ea5b73a5b0
mohammad sadra nafe <sadranafe7@gmail.com>
2025-12-09 18:20:06 +0330

    feat(auth-forms-validation): validate the form of the login page and set token in the localStorage and redirect users into their dashboard

--- week19-adminPanel/src/pages/auth/Login.jsx
+++ week19-adminPanel/src/pages/auth/Login.jsx
@@ -1,16 +1,59 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Form from "../../components/Form";
+import { useFormik } from "formik";
+import { authFormSchema } from "../../utils/authFormSchema";
+import axios from "axios";
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const formik = useFormik({
+        initialValues : {
+            userName : '',
+            password : '',
+        },
+        validationSchema : authFormSchema(false),
+        onSubmit : val => {
+            axios.post('http://localhost:3000/auth/login' , {
+                'username' : JSON.stringify(val.userName),
+                'password' : JSON.stringify(val.password)
+            })
+            .then(res => {
+                console.log(res)
+                localStorage.setItem('token' , res.data.token)
+                navigate('/dashboard')
+            })
+            .catch(err => {
+                const status = err.response.status;
+                switch(status) {
+                    case 400 : {
+                        console.error('invalid credentials')
+                        break;
+                    }
+
+                    case 401 : {
+                        console.log('Wrong username or password')
+                        break;
+                    }
+
+                    case 500 : {
+                        console.error('server errors')
+                        break;
+                    }
+                }
+            })
+        }
+    })
+
     return (
         <Form titleForm = 'فرم ورود'>
             <div className = "w-full flex flex-col flex-wrap justify-center items-center content-center gap-3">
-                <input type = "text" placeholder = "نام کاربری" className = "border border-white bg-neutral-100/60 backdrop-blur-xl rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.05)] outline-none p-2 px-3 w-10/12"/>
-                <input type = "password" placeholder = "رمز عبور" className = "border border-white bg-neutral-100/60 backdrop-blur-xl rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.05)] outline-none p-2 px-3 w-10/12"/>
+                <input type = "text" id = "userName" name = "userName" {...formik.getFieldProps('userName')} placeholder = "نام کاربری" className = "border border-white bg-neutral-100/60 backdrop-blur-xl rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.05)] outline-none p-2 px-3 w-10/12"/>
+                <input type = "password" id = "password" name = "password" {...formik.getFieldProps('password')} placeholder = "رمز عبور" className = "border border-white bg-neutral-100/60 backdrop-blur-xl rounded-lg shadow-[0_5px_10px_rgba(0,0,0,0.05)] outline-none p-2 px-3 w-10/12"/>
             </div>
 
             <div className = "w-full flex flex-wrap justify-center items-center gap-4">
-                <button type = "submit" className = "text-white bg-blue-500 rounded-lg py-2 w-10/12">ورود</button>
+                <button type = "submit" onClick = { formik.handleSubmit } className = "text-white bg-blue-500 rounded-lg py-2 w-10/12">ورود</button>
                 <Link to = '/auth/register' className = "text-blue-500 text-xs transition-all border-b border-transparent hover:border-b-blue-400">ایجاد حساب کاربری !</Link>
             </div>
         </Form>
--- week19-adminPanel/src/pages/auth/Register.jsx
+++ week19-adminPanel/src/pages/auth/Register.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useFormik } from "formik";
 import * as yup from 'yup';
 import Form from "../../components/Form";
+import { authFormSchema } from "../../utils/authFormSchema";
 
 const Register = () => {
     const navigate = useNavigate();
@@ -13,29 +14,21 @@ const Register = () => {
             password : '',
             repeatedPassword : '',
         },
-        validationSchema: yup.object({
-            userName : yup.string()
-            .min(3 , 'Must be at least 3 characters or more')
-            .max(15 , 'Must be 15 characters or less')
-            .required('userName is required'),
-            password : yup.string()
-            .min(8 , 'Password must be at least 8 characters')
-            .matches(/[0-9]/, "Password must contain at least one number")
-            .matches(/[a-zA-Z]/, "Password must contain at least one letter")
-            .required('password is required'),
-            repeatedPassword : yup.string()
-            .oneOf([yup.ref('password')] , 'Passwords do not match')
-            .required('confirm password is required')
-        }),
+        validationSchema: authFormSchema(true),
         onSubmit : val => {
             axios.post('http://localhost:3000/auth/register' , {
-                'username' : JSON.stringify(val.userName),
-                'password' : JSON.stringify(val.password),
+                username : JSON.stringify(val.userName),
+                password : JSON.stringify(val.password),
             })
-            .then(navigate('/auth/login' , { replace : true }))
+            .then((res) => {
+                console.log(res)
+                if(res.statusText === "Created" && res.status === 201){
+                    navigate('/auth/login' , { replace : true })
+                }
+            })
             .catch(err => {
                 if(!err.response) {
-                    console.error('Network error')
+                    console.error(err)
                     return;
                 }
                 const status = err.response.status;
