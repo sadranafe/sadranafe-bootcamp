eda3152ff547a15af3342417acf4ec5968421bb9
mohammad sadra nafe <sadranafe7@gmail.com>
2025-12-14 02:36:33 +0330

    feat(dashboard): add navbar to the dashboard layout

---
+++ week19-adminPanel/src/components/dashboard/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className = 'w-full flex flex-wrap justify-between items-center bg-white/25 border border-white shadow-[0_3px_10px_rgba(0,0,0,0.05)] rounded-xl px-3 p-1'>
+                <div className = 'w-10/12 flex items-center bg-re d-200 border-l border-neutral-200'>
+                    <i className = "ph-light ph-magnifying-glass text-2xl"></i>
+                    <input type = "text" className = 'w-full p-2 caret-blue-500 text-blue-600 placeholder:text-neutral-400 selection:bg-black selection:text-white' placeholder = 'جستجو کالا'/>
+                </div>
+
+                <div className = 'w-2/12 bg-bl ue-200 flex flex-wrap justify-center items-center gap-2'>
+                    <div className = 'flex flex-wrap justify-center items-center text-gray-500 rounded-full h-9 w-9 overflow-hidden'>
+                        <i className = "ph-light ph-user text-2xl"></i>
+                    </div>
+
+                    <div>
+                        <p className = 'font-semibold'>محمد صدرا نافع</p>
+                        <p className = 'text-neutral-500'>مدیر</p>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
--- week19-adminPanel/src/pages/Dashboard.jsx
+++ week19-adminPanel/src/pages/Dashboard.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import Navbar from "../components/dashboard/navbar";
 
 const Dashboard = () => {
     return (
         <>
-            
+            <div className = "w-9/12 mx-auto my-5">
+                <Navbar/>
+            </div>
         </>
     );
 };
