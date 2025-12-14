0e27e718ed940d2c14edb41f906e9319df32968c
mohammad sadra nafe <sadranafe7@gmail.com>
2025-12-14 10:48:18 +0330

    feat(dashboard): add header products of the dashboard

---
+++ week19-adminPanel/src/components/dashboard/Layout.jsx
@@ -0,0 +1,13 @@
+import ProductsHeader from "./ProductsHeader";
+
+const Layout = () => {
+    return (
+        <div className = "my-10">
+            <ProductsHeader/>
+
+
+        </div>
+    );
+};
+
+export default Layout;
--- week19-adminPanel/src/pages/Dashboard.jsx
+++ week19-adminPanel/src/pages/Dashboard.jsx
@@ -1,10 +1,13 @@
 import Navbar from "../components/dashboard/navbar";
+import Layout from "../components/dashboard/Layout";
 
 const Dashboard = () => {
     return (
         <>
             <div className = "w-9/12 mx-auto my-5">
                 <Navbar/>
+
+                <Layout/>
             </div>
         </>
     );
