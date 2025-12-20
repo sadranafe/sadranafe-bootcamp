a0a92205ec526a85f436db0f01a67194ef5fd995
mohammad sadra nafe <sadranafe7@gmail.com>
2025-12-20 19:23:57 +0330

    fix(pagination): edit the condition of the disabled buttons

--- week19-adminPanel/src/components/dashboard/ProductsPagination.jsx
+++ week19-adminPanel/src/components/dashboard/ProductsPagination.jsx
@@ -1,7 +1,7 @@
 const ProductsPagination = ({ page , setPage , totalPages }) => {
     return (
         <div className = "flex items-center justify-center px-2 gap-2">
-            <button onClick = {() => setPage(prev => prev - 1)} className = "flex justify-center items-center cursor-pointer p-2 disabled:text-neutral-400 disabled:cursor-default" disabled = {page === 1}>
+            <button onClick = {() => setPage(prev => prev - 1)} title = "صفحه قبلی" className = "flex justify-center items-center cursor-pointer p-2 disabled:text-neutral-400 disabled:cursor-default" disabled = {page ? page === 1 ? true : false : true}>
                 <i className = "ph-light ph-arrow-right text-lg"></i>
             </button>
 
@@ -9,7 +9,7 @@ const ProductsPagination = ({ page , setPage , totalPages }) => {
                 صفحه <p className = "border border-gray-400 h-8 px-2 pt-1 rounded-lg text-lg">{!page ? 1 : page}</p> از  <button onClick = {() => setPage(totalPages)} className = "cursor-pointer text-lg h-8 px-2 pt-1 rounded-lg">{!totalPages ? 1 : totalPages}</button>
             </div>
 
-            <button onClick = {() => setPage(prev => prev + 1)} className = "flex justify-center items-center cursor-pointer p-2 disabled:text-neutral-400 disabled:cursor-default" disabled = {page === totalPages}>
+            <button onClick = {() => setPage(prev => prev + 1)} title = "صفحه بعدی" className = "flex justify-center items-center cursor-pointer p-2 disabled:text-neutral-400 disabled:cursor-default" disabled = {page === totalPages}>
                 <i className = "ph-light ph-arrow-left text-lg"></i>
             </button>
         </div>
