7032fa8f10e74be0f3c7900068b7c44d4f579f3b
mohammad sadra nafe <sadranafe7@gmail.com>
2025-12-26 01:53:22 +0330

    feat: defines the routes of the project

---
+++ week21-adminpanel/src/app/auth/login/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+            this is login
+        </>
+    );
+};
+
+export default Page;
---
+++ week21-adminpanel/src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+const Page = () => {
+    return (
+        <>
+            <h1>THis is dashboard JS</h1>
+        </>
+    );
+};
+
+export default Page;
